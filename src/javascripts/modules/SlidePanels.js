0973881e4ae676aaa15aacc448397bebbe800465
Chloe Hwang <c.chloeh@gmail.com>
2019-01-10 17:57:30 -0500

    tidy JS

---
+++ src/javascripts/modules/SlidePanels.js
@@ -0,0 +1,52 @@
+export default class SlidePanels {
+  constructor(el) {
+    this.el = el
+    this.init()
+    this.bindEvents()
+  }
+
+  init() {
+    this.panels    = [].slice.call(this.el.getElementsByClassName('panel'));
+    this.panelsMap = {}
+    this.currId;
+
+    this.panels.forEach((panel, i) => {
+      this.panelsMap[i] = panel
+
+      if (panel.classList.contains('-open')) {
+        this.currId = panel.dataset.id
+      }
+    })
+  }
+
+  bindEvents() {
+    this.panels.forEach(panel => panel.addEventListener('click', this.onClick))
+  }
+
+  onClick = (e) => {
+    let newId = parseInt(e.target.dataset.id)
+
+    if (newId !== this.currId) {
+      newId > this.currId ? this.slideOver(newId) : this.slideBack(newId)
+      this.setNewPanel(newId)
+    }
+  }
+
+  slideOver(newId) {
+    for (let i = this.currId; i < newId; i++) {
+      this.panelsMap[i].classList.add('-slide-left')
+    }
+  }
+
+  slideBack(newId) {
+    for (let i = (this.currId - 1); i >= newId; i--) {
+      this.panelsMap[i].classList.remove('-slide-left')
+    }
+  }
+
+  setNewPanel(newId) {
+    this.panelsMap[newId].classList.add('-open')
+    this.panelsMap[this.currId].classList.remove('-open')
+    this.currId = newId
+  }
+}
