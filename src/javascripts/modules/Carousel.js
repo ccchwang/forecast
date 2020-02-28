eddecc39fd762fe24ec94e364819b6aeaefc8443
Cho Hwang <chloe.hwang@viget.com>
2020-02-28 18:55:07 -0500

    adding march horoscopes and email signup

---
+++ src/javascripts/modules/Carousel.js
@@ -0,0 +1,26 @@
+import Siema from 'siema';
+
+export default class Carousel {
+  constructor(el) {
+    // set up new carousel
+    let Carousel = new Siema({
+      duration: 250,
+      loop: true
+    });
+
+    // play on autoloop
+    let autoloop = setInterval(() => Carousel.next(), 1500);
+
+    // add click handlers to buttons
+    el.querySelector('.carousel-prev')
+      .addEventListener('click', () => {
+        clearInterval(autoloop);
+        Carousel.prev();
+      });
+    el.querySelector('.carousel-next')
+      .addEventListener('click', () => {
+        clearInterval(autoloop);
+        Carousel.next();
+      });
+  }
+}
--- src/javascripts/modules/ForecastSelector.js
+++ src/javascripts/modules/ForecastSelector.js
@@ -1,10 +1,30 @@
 export default class ForecastSelector {
   constructor(el) {
     this.el = el;
-    this.currentlyShownSign = this.el.value;
+    this.isArchive = el.dataset.archive;
+    this.currentlyShownSign = el.value;
     this.currentlyShownElement;
     this.forecasts = [].slice.call(document.getElementsByClassName('forecast-signs'));
     this.forecastsMap = {};
+
+    if (!this.isArchive) {      
+      this.header = document.getElementById("forecast-header");
+      this.colors = {
+        'Aries': '#ffcfcf',
+        'Gemini': '#cfcbff',
+        'Taurus': '#eaffc5',
+        'Cancer': '#cdfdfb',
+        'Leo': '#ffcfcf',
+        'Virgo': '#eaffc5',
+        'Libra': '#cfcbff',
+        'Scorpio': '#cdfdfb',
+        'Sagittarius': '#ffcfcf',
+        'Capricorn': '#eaffc5',
+        'Aquarius': '#cfcbff',
+        'Pisces': '#cdfdfb',
+      }
+    }
+
     this.init();
   }
 
@@ -34,6 +54,10 @@ export default class ForecastSelector {
         this.currentlyShownSign = newValue;
         this.currentlyShownElement = this.forecastsMap[newValue];
         this.currentlyShownElement.classList.add('show');
+
+        if (!this.isArchive) {
+          this.header.style.background = this.colors[this.currentlyShownSign];
+        }
 
         window.scrollTo(0,0);
       }
